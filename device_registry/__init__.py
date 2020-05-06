f376e22c1bfc4764799d0727949c9434f9b918ad
Nehuen Beron <nehuenberon@gmail.com>
2020-05-06 19:17:37 -0300

    vversion test mensahjes desde el devide

--- device_registry/__init__.py
+++ device_registry/__init__.py
@@ -3,12 +3,13 @@ import markdown
 import os
 import shelve
 from flask_restful import Resource, Api, reqparse
+import csv
 #creo la instancia de Flask
 app = Flask(__name__)
 
 # Create the API
 api = Api(app)
-
+'''
 def get_db():
     db = getattr(g, '_database', None)
     if db is None:
@@ -20,7 +21,7 @@ def teardown_db(exception):
     db = getattr(g, '_database', None)
     if db is not None:
         db.close()
-
+'''
 
 @app.route("/")
 def hello():
@@ -35,47 +36,31 @@ def hello():
 
 class DeviceList(Resource):
     def get(self):
-        shelf = get_db()
-        keys = list(shelf.keys())
-
-        devices = []
-
-        for key in keys:
-            devices.append(shelf[key])
-
-        return {'message': 'Success', 'data': devices}, 200
+        return {'message': 'OK', 'ECU': '02,03,04'}, 200
     
     def post(self):
+
         parser = reqparse.RequestParser()
 
-        parser.add_argument('identifier', required=True)
-        parser.add_argument('name', required=True)
-        parser.add_argument('device_type', required=True)
-        parser.add_argument('controller_gateway', required=True)
-
+        parser.add_argument('identifier', required=True, help='este dato es obligatorio')
+        parser.add_argument('timeStamp', required=True)
+        parser.add_argument('ECU')
+        parser.add_argument('acelerometer')
+        parser.add_argument('GPS')
+        
         # Parse the arguments into an object
         args = parser.parse_args()
-
+        # this is in case we use shelves instead of the mighty all power CSV
+        '''
         shelf = get_db()
+        print(shelf)
         shelf[args['identifier']] = args
+        '''
+        with open("dump.csv", "a") as pepe:
+            w = csv.DictWriter(pepe, args.keys())
+            w.writerow(args)
+            
 
         return {'message': 'Device registered', 'data': args}, 201
 
-class Device(Resource):
-    def get(self, identifier):
-        shelf = get_db()
-
-        #si no existe el dato que devuelva 404 error
-        if not(identifier in shelf):
-            return{'message':'Device not found', 'data':{}}, 404
-        return {'message': 'Device found', 'data': shelf[identifier]}, 200
-    
-    def delete(self, identifier):
-        shelf = get_db()
-        if not(identifier in shelf):
-            return{'message':'Device not found', 'data':{}}, 404
-        del shelf[identifier]
-        return '', 204
-
-api.add_resource(DeviceList, '/devices')
-api.add_resource(Device, '/device/<string:identifier>')
+api.add_resource(DeviceList, '/data')
